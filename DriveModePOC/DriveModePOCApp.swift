6175f7d5cd5d457824cb101f5909586d025851cc
Pradip <pradip.deore@logituit.com>
2024-11-26 13:07:22 +0530

    Initial Commit

---
+++ DriveModePOC/DriveModePOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriveModePOCApp.swift
+//  DriveModePOC
+//
+//  Created by Pradip on 26/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DriveModePOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
